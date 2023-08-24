e865d986246fe48d5e471ca16f544cb1a7c066e9
Makanjuola Elijah <makanjuolaelijah@gmail.com>
2023-08-24 14:30:29 +0100

    Adding the thymeleaf with css  tutorial

---
+++ src/test/java/com/luv2code/thmyleafdemo/ThmyleafdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.thmyleafdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThmyleafdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
